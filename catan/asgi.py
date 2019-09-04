ec3de1b6420334010f2f72f3e8c4a1f94f67ac2a
erbarbar <filipelopez3@gmail.com>
2019-09-04 01:04:49 +0100

    add log on socket connection

--- catan/asgi.py
+++ catan/asgi.py
@@ -3,6 +3,6 @@ import django
 from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "catan.settings")
-os.environ['ASGI_THREADS']="4"
+os.environ['ASGI_THREADS']="20"
 django.setup()
 application = get_default_application()
