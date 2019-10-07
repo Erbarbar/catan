ba7abc8f44ccadedb95c806b48b07f0062b6928d
Erbarbar <filipelopez3@gmail.com>
2019-10-07 02:06:28 +0100

    fix form in view

--- game/views.py
+++ game/views.py
@@ -35,6 +35,8 @@ def room(request, room_name):
             room.numbers = form.cleaned_data['numbers']
             room.villagesX = form.cleaned_data['villagesX']
             room.villagesY = form.cleaned_data['villagesY']
+            room.red_villages_placed_x = form.cleaned_data['red_villages_placed_x']
+            room.red_villages_placed_y = form.cleaned_data['red_villages_placed_y']
             room.save()
             form.save()
     else:
